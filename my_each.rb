530d67579923d63c7390f6feaf8891ed95e3d4c0
Yuiko Grace Nakano <gracenak@gmail.com>
2020-07-13 17:44:26 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,16 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array) # put argument(s) here
+  if block_given?
+  i = 0
+  
+  while i < array.length
+  i = i+1
+  yeild(array[i])
+end
+     
+  array# code here
+end
+
+collection = [1, 2, 3, 4]
+my_each(collection) do |i|
+  i
 end
