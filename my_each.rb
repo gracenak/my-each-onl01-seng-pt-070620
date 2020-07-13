d82b85851cf744c244887df6f5ccfeef3f25277b
Yuiko Grace Nakano <gracenak@gmail.com>
2020-07-13 18:19:27 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,15 +1,16 @@
 def my_each(array)
+  if block_given?
   new_array = []
   i = 0
   while array.length > i
   i = i+1
   yeild(array[])
   end
-new_array
+  new_array
 end
 
 collection = [1, 2, 3, 4]
-def my_each(collection) do |i|
+my_each(collection) do |i|
   i = 0
 
   while i < array.length
