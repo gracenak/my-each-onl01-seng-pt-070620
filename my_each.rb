a7612af5a7884a22ca44cf889887645096c36f86
Yuiko Grace Nakano <gracenak@gmail.com>
2020-07-13 18:48:09 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,12 +1,12 @@
 def my_each(array)
   if block_given?
-  new_array = []
+  #new_array = []
   i = 0
   while array.length > i
-  i = i+1
+   i = i+1
   yeild(array[])
   end
-  new_array
+  array
   end
 end
 
